d75a210935f2aaf3d452a972874266749b17ddb8
Myryos <hboutonnet1@gmail.com>
2019-01-27 00:27:02 +0100

    kk

--- Assets/Scripts/Test/OpenCloseTest.cs
+++ Assets/Scripts/Test/OpenCloseTest.cs
@@ -4,26 +4,31 @@ using UnityEngine;
 
 public class OpenCloseTest : MonoBehaviour
 {
-    public Animator m_animator;
+    //	public Animator m_animator;
+	public bool IsClose;
     // Start is called before the first frame update
     void Start()
     {
-        
+		IsClose = true;
+		m_transform = GetComponent<Transform> ();
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
 
 	public void Open()
 	{
-		m_animator.SetTrigger ("Open");
+		//m_animator.SetTrigger ("Open");
+		m_transform.position = Vector3.Lerp (m_transform.position, new Vector3 (m_transform.position.x, m_transform.position.y, m_transform.position.z + 1.0f), 0.8f);
 
 	}
 	public void Close()
 	{
-		m_animator.SetTrigger ("Close");
+		///m_animator.SetTrigger ("Close");
+		m_transform.position = Vector3.Lerp (m_transform.position, new Vector3 (m_transform.position.x, m_transform.position.y, m_transform.position.z - 1.0f), 0.8f);
 	}
+
+	private Transform m_transform;
 }
