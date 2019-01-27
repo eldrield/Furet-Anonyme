18609d01305558791f050452f9fef3d3c0767fe6
Myryos <hboutonnet1@gmail.com>
2019-01-27 07:56:02 +0100

    Application texture

---
+++ Assets/Scripts/Utilities/MAterialsAppli.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MAterialsAppli : MonoBehaviour
+{
+    #region Public Members
+
+	public Material m_book;
+
+	public Material m_book2;
+
+	#endregion
+    void Start()
+    {
+		m_renderer = GetComponentsInChildren<Renderer>();
+		foreach(Renderer child in m_renderer)
+		{
+			if(child.tag == "Livre")
+			{
+				child.material = m_book;
+			}
+			if(child.tag == "Livre 2")
+			{
+				child.material = m_book2;
+			}
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private Renderer[] m_renderer;
+}
